c991b56d9e413805c5a63a3cf9daa6f6582796ca
Rehaan1 <rehaanmazid@gmail.com>
2023-08-23 20:36:50 +0530

    feat: delete and clear cart routes

--- orders-billings/api/routes/cart.js
+++ orders-billings/api/routes/cart.js
@@ -170,5 +170,135 @@ router.get('/',tokenCheck, (req, res) => {
         })
 })
 
+router.delete('/removeItem',tokenCheck, (req, res) => {
+
+    if (!req.body.cartItemId) {
+        return res.status(400).json({
+            message: "Missing Required Body Content"
+        })
+    }
+
+    const cartItemId = req.body.cartItemId
+
+    const userId = req.userId
+
+    dbUserPool.connect()
+        .then(client => {
+            client.query("BEGIN")
+                .then(() => {
+                    
+                    const query = format(
+                        "DELETE FROM cart WHERE cart_item_id = %L AND user_id = %L RETURNING *",
+                        cartItemId,
+                        userId
+                    )
+
+                    client.query(query)
+                        .then(result => {
+                           
+                            client.query("COMMIT")
+                            client.release()
+                            if (result.rowCount === 0) {
+                                
+                                return res.status(404).json({
+                                    message: "Item Not Found or User Unauthorized"
+                                })
+                            }
+
+                            return res.status(200).json({
+                                message: "Item Deleted Successfully",
+                                data: result.rows[0] 
+                            })
+
+                        })
+                        .catch(err => {
+                            client.query("ROLLBACK")
+                            client.release()
+                            console.log("Error: ", err)
+                            return res.status(500).json({
+                                message: "Query error",
+                                error: err
+                            })
+                        })
+                })
+                .catch(err => {
+                    console.log("Error: ", err)
+                    client.release()
+                    return res.status(500).json({
+                        message: "Database transaction error",
+                        error: err
+                    })
+                })
+        })
+        .catch(err => {
+            console.log(err)
+            return res.status(200).json({
+                message: "Database Connection Error",
+                error: err
+            })
+        })
+})
+
+
+router.delete('/clear',tokenCheck, (req, res) => {
+
+    const userId = req.userId
+
+    dbUserPool.connect()
+        .then(client => {
+            client.query("BEGIN")
+                .then(() => {
+                    
+                    const query = format(
+                        "DELETE FROM cart WHERE user_id = %L RETURNING *",
+                        userId
+                    )
+
+                    client.query(query)
+                        .then(result => {
+                           
+                            client.query("COMMIT")
+                            client.release()
+                            if (result.rowCount === 0) {
+                                
+                                return res.status(404).json({
+                                    message: "Cart not cleared or User Unauthorized"
+                                })
+                            }
+
+                            return res.status(200).json({
+                                message: "Cart cleared Successfully",
+                                data: result.rowCount 
+                            })
+
+                        })
+                        .catch(err => {
+                            client.query("ROLLBACK")
+                            client.release()
+                            console.log("Error: ", err)
+                            return res.status(500).json({
+                                message: "Query error",
+                                error: err
+                            })
+                        })
+                })
+                .catch(err => {
+                    console.log("Error: ", err)
+                    client.release()
+                    return res.status(500).json({
+                        message: "Database transaction error",
+                        error: err
+                    })
+                })
+        })
+        .catch(err => {
+            console.log(err)
+            return res.status(200).json({
+                message: "Database Connection Error",
+                error: err
+            })
+        })
+})
+
 
 module.exports = router
