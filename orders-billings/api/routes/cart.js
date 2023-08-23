29e6c6f8b23999d137235d14f4a4ae211031a01d
Rehaan1 <rehaanmazid@gmail.com>
2023-08-23 20:01:56 +0530

    feat: get and add item to cart

--- orders-billings/api/routes/cart.js
+++ orders-billings/api/routes/cart.js
@@ -7,11 +7,157 @@ const { dbUserPool } = require('../db/db')
 const tokenCheck = require('../middlewares/tokenCheck')
 
 
+router.post('/add',tokenCheck, (req,res) => {
+
+    if (!req.body.itemId) {
+        return res.status(400).json({
+            message: "Missing Required Body Content"
+        })
+    }
+
+    if (!req.body.quantity) {
+        return res.status(400).json({
+            message: "Missing Required Body Content"
+        })
+    }
+
+    const itemId = req.body.itemId
+    const quantity = req.body.quantity
+
+    const userId = req.userId
+
+    dbUserPool.connect()
+        .then(client => {
+            client.query("BEGIN")
+                .then(() => {
+                    
+                    const query = format(
+                        "SELECT * FROM cart WHERE item_id = %L AND user_id = %L",
+                        itemId,
+                        userId
+                    )
+
+                    client.query(query)
+                        .then(result => {
+                            
+                            if(result.rowCount > 0)
+                            {
+                                return res.status(200).json({
+                                    message: "Item already in cart. Please update quantity in cart if required"
+                                })
+                            }
+
+                            const query = format(
+                                "INSERT INTO cart (user_id, item_id, quantity) VALUES (%L, %L, %L)",
+                                userId,
+                                itemId,
+                                quantity
+                            )
+
+                            client.query(query)
+                                .then( result => {
+                                    
+                                    client.query("COMMIT")
+                                    client.release()
+
+                                    return res.status(200).json({
+                                        message: "Data Inserted Successfully"
+                                    })
+
+                                })
+                                .catch(err => {
+                                    client.query("ROLLBACK")
+                                    client.release()
+                                    console.log("Error: ", err)
+                                    return res.status(500).json({
+                                        message: "Query error",
+                                        error: err
+                                    })
+                                })
+                           
+                        })
+                        .catch(err => {
+                            client.query("ROLLBACK")
+                            client.release()
+                            console.log("Error: ", err)
+                            return res.status(500).json({
+                                message: "Query error",
+                                error: err
+                            })
+                        })
+
+                })
+                .catch(err => {
+                    console.log("Error: ", err)
+                    client.release()
+                    return res.status(500).json({
+                        message: "Database transaction error",
+                        error: err
+                    })
+                })
+        })
+        .catch(err => {
+            console.log(err)
+            return res.status(200).json({
+                message: "Database Connection Error",
+                error: err
+            })
+        })
+    
+})
+
+
 router.get('/',tokenCheck, (req, res) => {
 
-    return res.status(200).json({
-        message: "Cart Endpoint Up"
-    })
+    const userId = req.userId
+
+    dbUserPool.connect()
+        .then(client => {
+            client.query("BEGIN")
+                .then(() => {
+                    
+                    const query = format(
+                        "SELECT * FROM cart WHERE user_id = %L",
+                        userId
+                    )
+
+                    client.query(query)
+                        .then(result => {
+                            
+                            client.query("COMMIT")
+                            client.release()
+                           
+                            return res.status(200).json({
+                                message: "User cart fetched successfully",
+                                data: result.rows[0] 
+                            })
+                        })
+                        .catch(err => {
+                            client.query("ROLLBACK")
+                            client.release()
+                            console.log("Error: ", err)
+                            return res.status(500).json({
+                                message: "Query error",
+                                error: err
+                            })
+                        })
+                })
+                .catch(err => {
+                    console.log("Error: ", err)
+                    client.release()
+                    return res.status(500).json({
+                        message: "Database transaction error",
+                        error: err
+                    })
+                })
+        })
+        .catch(err => {
+            console.log(err)
+            return res.status(200).json({
+                message: "Database Connection Error",
+                error: err
+            })
+        })
 })
 
 
